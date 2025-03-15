e695aca7821c4391f4388a21ffef57da41069c71
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2025-03-15 22:42:35 +0530

    Market Colour Theme

--- Farmisto-Frontend/src/utils/axios.jsx
+++ Farmisto-Frontend/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.production.VITE_API_BASE_URL;
-const condition = import.meta.env.production.VITE_NODE;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const condition = import.meta.env.VITE_NODE;
 
 const instance = axios.create({
   baseURL: condition === 'development' ? API_BASE_URL : 'https://farmisto.onrender.com',
