49ca8686c58b2895722a0574d0093d384581aad3
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2026-03-14 22:25:34 +0530

    MongoDB Issue Fix

--- Farmisto-Backend/config/Db.js
+++ Farmisto-Backend/config/Db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-const MongooseConnect = () => {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('Connected to MongoDB');
-    }).catch((err) => {
-        console.log('Error connecting to MongoDB', err);
-    });
-}
+const MongooseConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 module.exports = MongooseConnect;
